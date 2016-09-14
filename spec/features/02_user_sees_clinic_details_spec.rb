4d395ec17c4bf3755528a839c9bdb2af7b0ca6a6
Nick Alberts <nwalberts5@gmail.com>
2016-09-14 14:11:42 -0400

    nothing

--- spec/features/02_user_sees_clinic_details_spec.rb
+++ spec/features/02_user_sees_clinic_details_spec.rb
@@ -10,11 +10,11 @@ require 'spec_helper'
 #   [ ] I can click a link from the index page that leads me to the show page
 #   [ ] On the show page I can see details about the clinic and its comments
 #   [ ] I should see all of the the students that are attending the clinic
-#   [ ] I should not see details / reviews of other clinics
+#   [ ] I should not see details / comments of other clinics
 
 feature "User views a clinics show page" do
 
-  scenario "user sees a clinics name, speaker, description, and reviews" do
+  scenario "user sees a clinics name, speaker, description, and comments" do
     clinic_1 = Clinic.create(name: "Advanced Metaphysical Un-Object Programming", description: "Ugghh wut?", speaker: "Steven Universe")
     clinic_2 = Clinic.create(name: "Big-U Notation", description: "Big O notation but with the letter U", speaker: "Craig Barstow")
 
@@ -30,12 +30,12 @@ feature "User views a clinics show page" do
     expect(page).to have_content clinic_1.name
     expect(page).to have_content clinic_1.description
     expect(page).to have_content student.name
-    expect(page).to have_content review_1.username
-    expect(page).to have_content review_1.body
+    expect(page).to have_content comment_1.username
+    expect(page).to have_content comment_1.body
 
     expect(page).to_not have_content clinic_2.name
     expect(page).to_not have_content clinic_2.description
-    expect(page).to_not have_content review_2.username
-    expect(page).to_not have_content review_2.body
+    expect(page).to_not have_content comment_2.username
+    expect(page).to_not have_content comment_2.body
   end
 end
