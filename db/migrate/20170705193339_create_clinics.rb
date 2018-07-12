f5632de538241ea9116f91922ba1105aaa614a39
Nick Alberts <nwalberts5@gmail.com>
2018-07-12 18:05:27 -0400

    foo

--- db/migrate/20170705193339_create_clinics.rb
+++
@@ -1,9 +0,0 @@
-class CreateClinics < ActiveRecord::Migration[5.1]
-  def change
-    create_table :clinics do |t|
-      t.string :speaker, null: false
-      t.string :title, null: false
-      t.string :description
-    end
-  end
-end
